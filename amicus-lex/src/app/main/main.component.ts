b14e164e33359f4ab55025eb9369fcbf956b0aa7
Ravivarman Perinpanathan <ravivarman.perinpanathan@epitech.eu>
2018-07-10 14:26:28 +0200

    final

--- amicus-lex/src/app/main/main.component.ts
+++ amicus-lex/src/app/main/main.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Http, Response } from '@angular/http';
 import {MatDialog} from '@angular/material';
 import {DetailsComponent} from '../details/details.component';
+import {xml2json} from '../../../node_modules/xml2js';
 
 @Component({
   selector: 'app-main',
@@ -13,10 +14,16 @@ export class MainComponent implements OnInit {
   private apiUrl = 'https://address-book-demo.herokuapp.com/api/contacts';
   data: any = {};
   result: any ={};
+  companies: any = {};
+  appetizers: any = [];
+  SOURCE_FILE = 'https://www.senat.fr/rss/rapports.xml';
+  private _url= 'assets/sena.json';
 
   constructor(private http: Http, public dialog: MatDialog){
     this.getContacts();
     this.getData();
+    this.getTransform();
+    this.getsena();
   }
 
   openDialog() {
@@ -29,6 +36,21 @@ export class MainComponent implements OnInit {
       //this.result = result;
     //});
   }
+
+
+
+	getTransform() {
+    return this.http.get(this._url)
+      .map((response: Response) => response.json());
+  }
+
+  getsena(){
+    this.getTransform().subscribe(data =>{
+      console.log(data);
+      this.data = data
+    })
+  }
+
 
   getData(){
     return this.http.get(this.apiUrl)
